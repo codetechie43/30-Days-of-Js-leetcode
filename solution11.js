45e52bebe20acbb3c39a36a10bc7f1f9e5c7dc2a
codetechie43 <sujalkhade676@gmail.com>
2024-04-20 01:36:35 +0530

    2623. Memoize

---
+++ solution11.js
@@ -0,0 +1,11 @@
+function memoize(fn) {
+    const cache = {};
+
+    return function(...args) {
+        const key = JSON.stringify(args);
+        if (cache[key] === undefined) {
+            cache[key] = fn(...args);
+        }
+        return cache[key];
+    };
+}
